29d3232471f970e4f2680e238ccb829f5fb3d296
tbaums <michael.a.tanenbaum@gmail.com>
2018-09-25 10:48:47 -0400

    switch to python 3

--- project/app.py
+++ project/app.py
@@ -9,8 +9,4 @@ def homepage():
     return render_template('homepage.html')
 
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-
-    application.debug = True
-    application.run()
+    application.run(host="0.0.0.0")
